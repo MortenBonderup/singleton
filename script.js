a7fe3600c4e1bc11435899c9443ca8383c8e7212
Morten Bonderup <moab@eaaa.dk>
2024-09-30 14:47:26 +0200

    Now with children

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 let userProfile = {
     userrole: "admin",
     username: "Jane Doe",
-    yearsOfEmployment: 13
+    yearsOfEmployment: 13,
+    children: ["Anna", "George"]
 };
 
 // Function to display user profile
@@ -10,6 +11,10 @@ function displayUserProfile() {
     document.getElementById('username').textContent = `Username: ${userProfile.username}`;
     document.getElementById('userrole').textContent = `User Role: ${userProfile.userrole}`;
     document.getElementById('yearsOfEmployment').textContent = `Years of Employment: ${userProfile.yearsOfEmployment}`;
+    document.getElementById('children').textContent = "Children: ";
+    userProfile.children.forEach(child => {
+        document.getElementById('children').insertAdjacentText("beforeend", child + ";");
+    })
 }
 
 // Function to update user role
